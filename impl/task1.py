791bb415b7633e909c3f6c226205e931be7184f9
Soumya Shaw <37841510+soumyashaw@users.noreply.github.com>
2024-01-23 22:22:39 +0100

    Update task1.py

--- impl/task1.py
+++ impl/task1.py
@@ -1,9 +1,10 @@
 from agi.stk12.stkobjects import IAgSatellite, IAgSensor
+from agi.stk12.stkdesktop import AgESTKObjectType
 
 from spain.config import namespace
 from spain.stk import STK
 from spain.types import IntervalList
-from spain.util import inclinations
+from spain.util import inclinations, parse_stk_date, unparse_date_stk
 
 __all__ = [
     "initialise_scenario",
@@ -19,18 +20,59 @@ def initialise_scenario():
     STK.root.NewScenario("MoonColonisation")
 
     # Set analysis time period
-    # TODO
+    startTime = unparse_date_stk(namespace.start)
+    endTime = unparse_date_stk(namespace.stop)
+
+    STK.root.UnitPreferences.SetCurrentUnit("DateFormat", "UTCG")
+    STK.root.CurrentScenario.SetTimePeriod(startTime, endTime)
+
 
     # Reset to analysis start time
     STK.root.Rewind()
 
     # Create moon colonies
-    # TODO create colony A
+    # Colony A
+    targetNameA = "MoonBaseA"
+    moonLatitudeA = namespace.base_A.lat
+    moonLongitudeA = namespace.base_A.lon
+    moonAltitudeA = namespace.base_A.alt
 
-    # TODO create colony B
+    targetA = STK.root.CurrentScenario.Children.New(AgESTKObjectType.eTarget, targetNameA)
+    targetA.Position.AssignGeodetic(moonLatitudeA, moonLongitudeA, moonAltitudeA)
+    targetA.CentralBody = 'Moon'
+
+    # Colony B
+    targetNameB = "MoonBaseB"
+    moonLatitudeB = namespace.base_B.lat
+    moonLongitudeB = namespace.base_B.lon
+    moonAltitudeB = namespace.base_B.alt
+
+    targetB = STK.root.CurrentScenario.Children.New(AgESTKObjectType.eTarget, targetNameB)
+    targetB.Position.AssignGeodetic(moonLatitudeB, moonLongitudeB, moonAltitudeB)
+    targetB.CentralBody = 'Moon'
+
 
     # Create DSN ground stations
-    # TODO
+    facilityName1 = "DSS_14_Goldstone_STDN_DS14"
+    latitude1 = 35.425901
+    longitude1 = -116.889538
+    altitude1 = 1001.39
+    facility1 = STK.root.CurrentScenario.Children.New(AgESTKObjectType.eFacility, facilityName1)
+    facility1.Position.AssignGeodetic(latitude1, longitude1, altitude1)
+
+    facilityName2 = "DSS_43_Tidbinbilla_STDN_DS43"
+    latitude2 = -35.402424
+    longitude2 = 148.981267
+    altitude2 = 688.867
+    facility2 = STK.root.CurrentScenario.Children.New(AgESTKObjectType.eFacility, facilityName2)
+    facility2.Position.AssignGeodetic(latitude2, longitude2, altitude2)
+
+    facilityName3 = "DSS_63_Robledo_STDN_DS63"
+    latitude3 = 40.43121
+    longitude3 = -4.248009
+    altitude3 = 864.816
+    facility3 = STK.root.CurrentScenario.Children.New(AgESTKObjectType.eFacility, facilityName3)
+    facility3.Position.AssignGeodetic(latitude3, longitude3, altitude3)
 
 
 def add_moon_satellite(inclination: int, name: str) -> tuple[IAgSatellite, IAgSensor]:
